5e63eaa1b70dba1910505082998fe1db11d4510e
Julián rodríguez Olaya <100172561+SHULIUS@users.noreply.github.com>
2023-04-22 19:06:06 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,8 +3,9 @@ import {createPool} from 'mysql2/promise'
 export const pool = createPool({
     host: 'localhost',
     user: 'root',
-    password: 'canetico1',
+    password: '',
     port: 3306,
-    database: 'companydb'
+    database: '
+   '
 })
 
